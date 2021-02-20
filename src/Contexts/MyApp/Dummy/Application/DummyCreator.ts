2d6f732577917254923d80a9911573f06e1c2cd0
Arnold <arnold9525@gmail.com>
2021-02-20 21:57:32 +0100

    First use case implementation.

--- src/Contexts/MyApp/Dummy/Application/DummyCreator.ts
+++ src/Contexts/MyApp/Dummy/Application/DummyCreator.ts
@@ -1,5 +1,10 @@
 import { inject, injectable } from "inversify";
+import { Dummy } from "../Domain/Dummy";
 import { DummyRepository } from "../Domain/DummyRepository";
+import { DummyAlreadyExist } from "../Domain/Exception/DummyAlreadyExist";
+import { DummyContent } from "../Domain/ValueObject/DummyContent";
+import { DummyId } from "../Domain/ValueObject/DummyId";
+import { DummyTitle } from "../Domain/ValueObject/DummyTitle";
 import { DummyCreatorRequest } from "./DummyCreatorRequest";
 @injectable()
 export class DummyCreator {
@@ -9,6 +14,17 @@ export class DummyCreator {
     }
 
     public async run(request: DummyCreatorRequest) {
-        console.log(request);
+        const id = new DummyId(request.id);
+        const title = new DummyTitle(request.title);
+        const content = new DummyContent(request.content);
+        await this.ensureDoesNotExist(id);
+        const dummy = Dummy.create(id, title, content);
+        await this.repository.create(dummy);
+    }
+
+    private async ensureDoesNotExist(id: DummyId) {
+        if (this.repository.find(id)) {
+            throw new DummyAlreadyExist('Dummy already exists');
+        }
     }
 }
--- src/Contexts/MyApp/Dummy/Domain/DummyRepository.ts
+++ src/Contexts/MyApp/Dummy/Domain/DummyRepository.ts
@@ -1,6 +1,7 @@
 import { Dummy } from "./Dummy";
+import { DummyId } from "./ValueObject/DummyId";
 
 export interface DummyRepository {
-    find(): Promise<Dummy>;
+    find(id: DummyId): Promise<Dummy>;
     create(dummy: Dummy): Promise<void>;
 }
