24e3e4c2f44243a9c782bec50f0064e0425e015b
Arnold <arnold9525@gmail.com>
2021-02-21 19:38:43 +0100

    initial connection to rabbitmq

--- src/Apps/MyApp/DependencyContainer/Containers/SharedContainer.ts
+++ src/Apps/MyApp/DependencyContainer/Containers/SharedContainer.ts
@@ -1,14 +1,18 @@
-import { InMemorySyncEventBus } from "@/Contexts/Shared/Infrastructure/EventBus/InMemory/InMemorySyncEventBus";
 import { ContainerTypes } from "../ContainerTypes";
+import { RabbitMQConnection } from "@/Contexts/Shared/Infrastructure/EventBus/RabbitMQ/RabbitMQConnection";
+import { RabbitMQEventBus } from "@/Contexts/Shared/Infrastructure/EventBus/RabbitMQ/RabbitMQEventBus";
 
 export class SharedContainer {
     public static getContainer(): ContainerTypes {
         return {
+            services: [
+                RabbitMQConnection
+            ],
             buses: [
                 {
                     abstract: 'EventBus',
-                    concrete: InMemorySyncEventBus
-                }
+                    concrete: RabbitMQEventBus
+                },
             ]
         };
     }
--- src/Contexts/Shared/Infrastructure/EventBus/RabbitMQ/RabbitMQEventBus.ts
+++ src/Contexts/Shared/Infrastructure/EventBus/RabbitMQ/RabbitMQEventBus.ts
@@ -1,21 +1,24 @@
-import { inject } from "inversify";
+import { inject, injectable } from "inversify";
 import { RabbitMQConnection } from "@/Contexts/Shared/Infrastructure/EventBus/RabbitMQ/RabbitMQConnection";
 import { DomainEvent } from "@/Contexts/Shared/Domain/Bus/Event/DomainEvent";
 import { EventBus } from "@/Contexts/Shared/Domain/Bus/Event/EventBus";
 import { DomainEventSubscriber } from "@/Contexts/Shared/Domain/Bus/Event/DomainEventSubscriber";
 
-export class RabbitMQEventBus implements EventBus{
+@injectable()
+export class RabbitMQEventBus implements EventBus {
+    private readonly exchangeName: string;
+
     public constructor(
-        @inject('RabbitMQConnection') private readonly connection: RabbitMQConnection,
-        private exchangeName: string
+        @inject('RabbitMQConnection') private readonly connection: RabbitMQConnection
     ) {
+        this.exchangeName = process.env.RABBITMQ_EXCHANGE;
     }
 
     public addSubscribers(subscribers: DomainEventSubscriber<DomainEvent>[]): void {
-        
+
     }
 
-    public async publish(events: DomainEvent[]):Promise<void> {
+    public async publish(events: DomainEvent[]): Promise<void> {
         events.map(async event => {
             await this.publisher(event);
         })
