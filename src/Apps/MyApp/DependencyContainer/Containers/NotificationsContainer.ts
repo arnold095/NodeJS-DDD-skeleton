aef62dece854017c3cdae44c25e2a50876e829a5
Arnold <arnold9525@gmail.com>
2021-02-21 12:24:10 +0100

    NodeMailer implementation in sync event bus.

---
+++ src/Apps/MyApp/DependencyContainer/Containers/NotificationsContainer.ts
@@ -0,0 +1,20 @@
+import { SendWelcomeDummyMail } from "@/Contexts/Notifications/Mail/Application/SendWelcomeDummyMail";
+import { SendWelcomeDummyMailOnDummyCreated } from "@/Contexts/Notifications/Mail/Application/SendWelcomeDummyMailOnDummyCreated";
+import { NodeMailer } from "@/Contexts/Notifications/Mail/Infrastructure/NodeMailer";
+import { ContainerTypes } from "../ContainerTypes";
+
+export class NotificationsContainer {
+    public static getContainer(): ContainerTypes {
+        return {
+            services: [
+                SendWelcomeDummyMail, SendWelcomeDummyMailOnDummyCreated
+            ],
+            repositories: [
+                {
+                    abstract: 'MailSender',
+                    concrete: NodeMailer
+                }
+            ]
+        }
+    }
+}
