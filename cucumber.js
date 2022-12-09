d8bce28e5de711285dcef97be178a16299a2b523
arnold <arnold9525@gmail.com>
2022-12-09 12:01:42 +0100

    refactor: remove unused package

--- cucumber.js
+++ cucumber.js
@@ -1,9 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: './dev.test.env' });
-const common = [
-  '--require-module ts-node/register',
-  '--require-module tsconfig-paths/register',
-];
+const common = ['--require-module ts-node/register'];
+
 console.info('RUNNING ACCEPTANCE TESTS');
 const e2e = [...common, '**/tests/**/*.feature', '--require **/tests/**/*.steps.ts'].join(
   ' ',
